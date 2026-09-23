e10c3a5bf49e180d1b9f702511601bc70a3e9f73
ryutaro0222 <xrxr0222@gmail.com>
2026-09-23 23:09:07 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Google Cloud で作成した「OAuth クライアント ID」をここに貼り付けてください。
 // 例: "123456789012-abcdefg.apps.googleusercontent.com"
 window.READER_CONFIG = {
-  googleClientId: "ここにクライアントIDを貼り付け",
+  googleClientId: "950880130853-itv2qpneg5p35fh3igbjrod3b4r4hcid.apps.googleusercontent.com",
   // 本棚として使うGoogleドライブのフォルダ名（無ければ自動で作成されます）
   shelfFolderName: "見開きリーダー",
 };
